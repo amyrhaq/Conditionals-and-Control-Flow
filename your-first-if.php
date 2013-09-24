aba5ebb4bd5cafcbfebd0ffece2c5e17db25f808
Your Name Here <amyr@amyrhaq.com>
2013-09-23 19:29:23 -0700

    completed lesson 3/6

---
+++ your-first-if.php
@@ -0,0 +1,17 @@
+<!-- Set the number of $items to a number greater than 5, 
+then complete the if statement by replacing the ___s with the correct syntax. -->
+<html>
+<head>
+	<title>Your First If</title>
+</head>
+<body>
+<p>
+<?php 
+$items = 10;
+if ($items > 5) {
+echo "You get a 10% discount";
+}
+ ?>
+</p>
+</body>
+</html>
