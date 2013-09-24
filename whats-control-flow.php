5eb4853f04b7ffb39b41e0a84a95ef410a05715e
Your Name Here <amyr@amyrhaq.com>
2013-09-23 18:06:57 -0700

    Completed lesson 1/6

---
+++ whats-control-flow.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title>What's Control Flow?</title>
+</head>
+<body>
+<p>
+	<?php 
+	$teabags = 3;
+	if ($teabags > 0)
+	{
+		echo "Let's have some tea";
+	} 
+	else 
+	{
+		echo "No more tea :(";
+	} 
+	?>
+</p>
+</body>
+</html>
