a3430d42f970c95cd9534ff73e1b046bc5eb240f
Your Name Here <amyr@amyrhaq.com>
2013-09-23 19:18:23 -0700

    completed lesson 2/6

---
+++ making-comparisons.php
@@ -0,0 +1,20 @@
+<!-- Go ahead and write a comparison that is true -->
+<html>
+<head>
+	<title>Making Comparisons</title>
+</head>
+<body>
+<p>
+<?php $coffee = 5;
+	if ($coffee > 0)
+	{
+		echo "More coffee please";
+	}
+	else
+	{
+		echo "No more coffee";
+	}
+?>
+</p>
+</body>
+</html>
